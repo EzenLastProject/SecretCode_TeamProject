e2e0a52d03f002fd8949f67742a3b70df113ddb9
tkwkduddnd <yongdam84@gmail.com>
2024-05-24 18:20:08 +0900

    admin_나영우3

--- www/src/main/java/SecretCode/ezen/www/controller/AdminController.java
+++ www/src/main/java/SecretCode/ezen/www/controller/AdminController.java
@@ -1,10 +1,12 @@
 package SecretCode.ezen.www.controller;
 
+import SecretCode.ezen.www.service.AdminService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/admin/*")
 public class AdminController {
+    private final AdminService asv;
+
+    @GetMapping("/adminBoard")
+    public void adminBoard(){}
 
 
+    @GetMapping("/adminUser")
+    public void adminUser(){}
+
+    @GetMapping("/adminRegister")
+    public void adminRegister(){}
 }
