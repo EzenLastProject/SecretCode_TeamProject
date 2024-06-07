d24dea8187fa6d02f79bee3a2d1c2116ebe7c01e
tkwkduddnd <yongdam84@gmail.com>
2024-06-07 17:00:05 +0900

    admin_나영우240607

--- www/src/main/java/SecretCode/ezen/www/controller/AdminController.java
+++ www/src/main/java/SecretCode/ezen/www/controller/AdminController.java
@@ -1,13 +1,17 @@
 package SecretCode.ezen.www.controller;
 
+import SecretCode.ezen.www.domain.QnaVO;
 import SecretCode.ezen.www.service.AdminService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 @Controller
 @Slf4j
@@ -16,16 +20,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
     private final AdminService asv;
 
-    @GetMapping("/adminBoard")
-    public void adminBoard(){}
+    @GetMapping("/adminDetail")
+    public String adminDetail(@RequestParam("bno") int bno, Model m) {
+        QnaVO list = asv.getAdminDetail(bno);
+        m.addAttribute("list", list);
+        return "/adminRegister/adminDetail";
+    }
+
+    @DeleteMapping(value="/admin/{bno}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> delete(@PathVariable("bno")int bno){
+        log.info("bno {}", bno);
+        int isOk = asv.deleteQna(bno);
+
+        return isOk > 0 ? new ResponseEntity<>("1", HttpStatus.OK) :
+                new ResponseEntity<>("0", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 
-    @GetMapping("/adminUser")
-    public void adminUser(){}
-
-    @GetMapping("/adminRegister")
-    public void adminRegister(){}
-
-    @GetMapping("/adminReservation")
-    public void adminReservation(){}
 }
--- www/src/main/java/SecretCode/ezen/www/service/AdminServiceImpl.java
+++ www/src/main/java/SecretCode/ezen/www/service/AdminServiceImpl.java
@@ -1,5 +1,6 @@
 package SecretCode.ezen.www.service;
 
+import SecretCode.ezen.www.domain.QnaVO;
 import SecretCode.ezen.www.repository.AdminMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,4 +11,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService{
     private final AdminMapper adminMapper;
+
+
+    @Override
+    public QnaVO getAdminDetail(int bno) {
+        return adminMapper.getAdminDetail(bno);
+    }
+
+    @Override
+    public int deleteQna(int bno) {
+        return adminMapper.deleteQna(bno);
+    }
 }
